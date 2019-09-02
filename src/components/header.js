9dbe4f1c5e25beedb30b990e938fb2d355d0a6e0
falihnaufal17 <dudu.kotkot@gmail.com>
2019-09-02 19:23:34 +0700

    add dropdown ascending descending

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,60 @@
 import React, { Component } from 'react'
 import { Container, Header, Left, Body, Right, Button, Icon, Title, Text } from 'native-base';
+import Menu, { MenuItem, MenuDivider } from 'react-native-material-menu';
 import { withNavigation } from 'react-navigation'
+import axios from 'axios'
+import url from '../../Api';
+
 export class AppBar extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            notes: []
+        }
+    }
+    _menu = null;
+
+    setMenuRef = ref => {
+        this._menu = ref
+    }
+
+    hideMenu = () => {
+        this._menu.hide()
+    }
+
+    showMenu = () => {
+        this._menu.show()
+    }
+
+    componentDidMount = async () => {
+        await this.getNoteAscending()
+        await this.getNoteDescending()
+    }
+
+    getNoteAscending() {
+        axios.get(`${url}/notes`)
+            .then((res) => {
+                this.setState({
+                    notes: res.data.result
+                })
+            })
+            .catch((error) => {
+                alert('oops something wrong!')
+            })
+    }
+
+    getNoteDescending() {
+        axios.get(`${url}/notes/desc`)
+            .then((res) => {
+                this.setState({
+                    notes: res.data.result
+                })
+            })
+            .catch((error) => {
+                alert('oops something wrong!')
+            })
+    }
+
     render() {
         return (
             <Header style={{ backgroundColor: 'light' }}>
@@ -14,9 +67,15 @@ export class AppBar extends Component {
                     <Title style={{ color: '#000000' }}>NoteApp</Title>
                 </Body>
                 <Right>
-                    <Button transparent>
-                        <Icon name='funnel' style={{ color: '#000000' }} type='Ionicons' />
-                    </Button>
+                    <Menu
+                        style={{ flex: 1, alignSelf: 'flex-end' }}
+                        ref={this.setMenuRef}
+                        button={<Text onPress={this.showMenu}><Icon name='funnel' style={{ color: '#000000' }} type='Ionicons' /></Text>}
+                    >
+                        <MenuItem onPress={this.hideMenu}>ASCENDING</MenuItem>
+                        <MenuDivider />
+                        <MenuItem onPress={this.hideMenu}>DESCENDING</MenuItem>
+                    </Menu>
                 </Right>
             </Header>
         )
